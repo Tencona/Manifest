36f51c935bf7010befe13b91fc3dd7bc36876168
Tencona <ctennery.dev@gmail.com>
2019-04-28 11:54:33 -0500

    Property, Item, Dashboard stubs
    Added stubs for:
    - components/Property.vue
    - views/Dashboard.vue
    - views/Item.vue

---
+++ src/components/Property.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="Property"></div>
+</template>
+
+<script>
+export default {
+	name: "Property",
+	components: {},
+	props: {
+		property: Object,
+		index: Number
+	},
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {}
+};
+</script>
+
+<style scoped>
+.Property {
+}
+</style>
---
+++ src/views/Item.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="Item">
+		<div id="headerBar">
+			<span id="title">item.name</span>
+			<span id="type">item.type</span>
+			<span id="uuid">item.uuid</span>
+		</div>
+		<div id="body">
+			<div class="propertiesWrapper">
+				<div class="properties">
+					<DiaryEntry
+						class="property"
+						v-for="(property, i) in item.type.properties"
+						:key="i"
+						v-bind:entry.sync="property"
+						:index="i"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Item",
+	components: {},
+	props: {
+		item: Object
+	},
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {}
+};
+</script>
+
+<style scoped>
+.Item {
+}
+</style>
