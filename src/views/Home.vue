5e19644ad6a9880a0a88cc303ca5138066cb6a8a
Tencona <ctennery.dev@gmail.com>
2019-04-29 01:23:05 -0500

    Added button for generating random Manifest data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<button @click.native="generateData">Generate Data</button>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import manifest from "../Manifest/manifest.js";
+import gendata from "../Manifest/data/generate-test-data";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "home",
+	components: {},
+	mounted() {},
+	methods: {
+		generateData() {
+			gendata.init(manifest);
+			gendata.generate();
+			var s = "";
+		}
+	}
+};
 </script>
