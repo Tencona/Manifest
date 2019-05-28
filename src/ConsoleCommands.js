8d912aa0ae692c60ea50189b2395c1c91838e28e
Conner Tennery <ctennery.dev@gmail.com>
2019-05-28 02:51:26 -0500

    Added ConsoleCommands.js and basic stub

---
+++ src/ConsoleCommands.js
@@ -0,0 +1,15 @@
+export { exists };
+
+function executeCommand(userInput) {
+    //search -item "search text"
+        //Matches on item name or uuid
+
+    //search -type "search text"
+        //Matches on type name or uuid
+        
+    //search -item -<property name> -<property value> -<property name 2> -<property value 2>
+        //Matches all itms where the given properties have the specified values
+
+    //search -type -<property name>  -<property name 2>
+        //Matches all types that have the given properties
+}
