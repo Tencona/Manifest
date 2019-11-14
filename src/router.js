ba04432c5b79532d64a7f6419113cde470538afa
Conner Tennery <ctennery.dev@gmail.com>
2019-11-13 21:07:18 -0600

    Added route to Item

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Dashboard from './views/Dashboard.vue';
+import Item from './views/Item.vue';
 import Console from './views/Console.vue';
 
 Vue.use(Router);
@@ -19,6 +20,11 @@ export default new Router({
 			path: '/dashboard',
 			name: 'dashboard',
 			component: Dashboard,
+		},
+		{
+			path: '/item',
+			name: 'item',
+			component: Item,
 		},
 		{
 			path: '/console',
