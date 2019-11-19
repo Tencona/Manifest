fd07a4afb679f6384e608f21ceca806f4672cdd0
Conner Tennery <ctennery.dev@gmail.com>
2019-11-19 08:27:12 -0600

    Added removeProperty to Type

--- src/Manifest/models/type.js
+++ src/Manifest/models/type.js
@@ -4,16 +4,12 @@ import { Result, RESULT_TYPE } from './result';
 import { exists, uuid } from '../extensions/utility';
 
 export default class Type {
-	//TODO Types need a Key! One Key Property that gets searched first before other things get searched. I am such a dumb dumb for not thinking of that sooner.
 	constructor(name) {
 		this.uuid = uuid();
 		this.name = name;
 		this.parentType = undefined;
 		this.pageLayout = undefined; //Unsure how to handle this.
 		this._properties = [];
-
-		//! This could be a bad idea. Circular reference and all.
-		// this.items = [];
 	}
 
 	get isValid() {
@@ -37,6 +33,29 @@ export default class Type {
 				}`
 			);
 		} else return new Result(RESULT_TYPE.Error, 'Attempted to add an invalid Property in Type.addProperty()');
+	}
+
+	removeProperty(property) {
+		if (property) {
+			let foundProperty = this._properties.findIndex(x => x.uuid === property.uuid);
+			if (foundProperty) {
+				this._properties.splice(foundProperty, 1);
+
+				return new Result(
+					RESULT_TYPE.Success,
+					`Successfully removed Property: ${property.uuid} from Type: ${this.uuid}`
+				);
+			} else {
+				return new Result(
+					RESULT_TYPE.Error,
+					`Attempted to remove a Property: ${property.uuid} that doesn't belong to Type: ${this.uuid}`
+				);
+			}
+		} else
+			return new Result(
+				RESULT_TYPE.Error,
+				`Attempted to remove a Property that doesn't exist in Type.removeProperty()'`
+			);
 	}
 
 	get properties() {
