5790d784bc495c7c7c7e6b60a955a754ca8ea8f9
Conner Tennery <ctennery.dev@gmail.com>
2019-11-19 08:27:41 -0600

    Stubbed item.removeUnusedProperties()

--- src/Manifest/models/item.js
+++ src/Manifest/models/item.js
@@ -25,4 +25,10 @@ export default class Item {
 				`Attempted to set Item Property with Property: '${property}' and Value: '${value}'`
 			);
 	}
+
+	removeUnusedProperties() {
+		//TODO this will check this Item's Type's Properties and if it contains a value in `this.properties` that isn't in the Type Properties, it will remove it.
+		//Have to do it here because if I do it on removing a property, I'll have to iterate down it and all of its children then look up all of the items under that Type line then remove the property from all of them.
+		//That's probably better, but Items are referenced one at a time 99% of the time, so it can perform a clean-up on loading.
+	}
 }
