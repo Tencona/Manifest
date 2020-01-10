ee48678d19d38459e4468d921cab847c2d576aab
Conner Tennery <ctennery.dev@gmail.com>
2020-01-10 15:11:39 -0600

    npm audit and added Credits

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<span>Nope. Nothing here yet except credits. Maybe I'll never add anything here.</span>
+		<h1>Credits</h1>
+		<p>
+			<a href="https://github.com/connertennery">Creator - Conner Tennery</a>
+			<br />
+			<a href="https://twitter.com/youyuxi">Vue - Evan You</a>
+			<br />
+			<a href="https://github.com/jbaysolutions/vue-grid-layout">Grid - JBay Solutions</a>
+		</p>
+	</div>
 </template>
