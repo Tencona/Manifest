8359f0471cbc35e9b030b1ca88420813aa45e0a0
Conner Tennery <ctennery.dev@gmail.com>
2020-01-17 15:15:38 -0600

    Added hotkeys base
    Hotkey system is in. Added small utility file for the app, outside of the Manifest library

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './registerServiceWorker';
+import store from './store';
 
-Vue.config.productionTip = false
+//My Garbage
+import utility from './support/utility';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: h => h(App),
+}).$mount('#app');
