cfe89f86bc41e6ef57404ef9df2d806fe241596a
Conner Tennery <ctennery.dev@gmail.com>
2020-03-27 14:47:11 -0500

    Removed useless exists function

--- src/Manifest/extensions/utility.js
+++ src/Manifest/extensions/utility.js
@@ -1,15 +1,8 @@
-export { exists, uuid };
-
-function exists(o) {
-	return !(typeof o === 'undefined');
-}
+export { uuid };
 
 //https://stackoverflow.com/a/2117523
 function uuid() {
 	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-		(
-			c ^
-			(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-		).toString(16)
+		(c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
 	);
 }
